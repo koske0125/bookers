f2ae1e9b2bbd0644aeab8fdf360af9ec849cd05b
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-05 01:14:13 +0000

    [Update]一覧ページ・投稿機能を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+
+  def list
+    @books = List.all
+    @list = List.new
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to '/books'
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root :to => 'homes#index' #ルートにアクセスするとhomesコントローラーのindexアクションを実行
+root :to => 'homes#index' #rootにアクセスするとhomesコントローラーのindexアクションを実行
+get '/books' => 'books#list' #/booksにアクセスするとbooksコントローラーのlistアクションを実行
+get '/books/:id' => 'books#show', as: 'book'
+get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+patch '/books/:id' => 'books#update', as: 'update_book'
+post 'books' => 'books#create'
+delete '/books/:id' => 'books#destroy', as: 'destroy_book'
 
 end
